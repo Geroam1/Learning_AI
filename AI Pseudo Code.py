6a4087e08448ebf09e40554d2ebb9d08a984d37f
Geroam1 c:; cd 'c:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2'; & 'C:\Users\speed\AppData\Local\Microsoft\WindowsApps\python3.11.exe' 'c:\Users\speed\.vscode\extensions\ms-python.python-2023.16.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '56397' '--' 'C:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2\HelloWorld2.py <502416@student.fontys.nl c:; cd 'c:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2'; & 'C:\Users\speed\AppData\Local\Microsoft\WindowsApps\python3.11.exe' 'c:\Users\speed\.vscode\extensions\ms-python.python-2023.16.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '56391' '--' 'C:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2\HelloWorld2.py>
2023-09-28 14:19:42 +0200

    Cool camera code

--- AI Pseudo Code.py
+++ AI Pseudo Code.py
@@ -75,17 +75,78 @@
 
 
 # example of an enviroment checking code
-real_enviroment = False
-enviroments = ["Dent", "South Bump", "North Bump", "West Bump", "East Bump"]
+# real_enviroment = False
+# enviroments = ["Dent", "South Bump", "North Bump", "West Bump", "East Bump"]
 
-while real_enviroment == False:
-    enviroment = input("state an enviroment: ")
-    if enviroment not in enviroments:
-        print("Please enter an actaul enviroment")
-    else:
-        real_enviroment = True
+# while real_enviroment == False:
+#     enviroment = input("state an enviroment: ")
+#     if enviroment not in enviroments:
+#         print("Please enter an actaul enviroment")
+#     else:
+#         real_enviroment = True
 
-print("thats a valid enviroment")
+# print("thats a valid enviroment")
+
+import cv2
+import numpy as np
+
+# Initialize the camera
+cap = cv2.VideoCapture(0)  # 0 represents the default camera (usually the built-in webcam)
+
+while True:
+    # Capture a frame from the camera
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Error: Unable to capture frame")
+        break
+
+    # Convert the frame to the HSV color space
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Define the range of red color in HSV
+    lower_red = np.array([0, 100, 100])  # Lower bound for red
+    upper_red = np.array([10, 255, 255])  # Upper bound for red
+
+    # Create a mask to extract the red color
+    mask = cv2.inRange(hsv_frame, lower_red, upper_red)
+
+    # Apply Gaussian blur to the mask to reduce noise
+    blurred_mask = cv2.GaussianBlur(mask, (9, 9), 2)
+
+    # Find contours in the blurred mask
+    contours, _ = cv2.findContours(blurred_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Loop through the detected contours
+    for contour in contours:
+        # Approximate the contour to a circle
+        approx = cv2.approxPolyDP(contour, 0.02 * cv2.arcLength(contour, True), True)
+
+        # If the contour is a circle (it should have very few vertices)
+        if len(approx) >= 8:
+            # Draw a bounding circle around the detected red circle
+            (x, y), radius = cv2.minEnclosingCircle(contour)
+            center = (int(x), int(y))
+            radius = int(radius)
+            cv2.circle(frame, center, radius, (0, 255, 0), 2)  # Draw a green circle
+
+    # Display the frame with detected circles
+    cv2.imshow("Red Circle Detection", frame)
+
+    # Exit the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
 
 
 
