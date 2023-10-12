0b432e02416272780fcf9cec1a3b908e47885ec7
Geroam1 c:; cd 'c:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2'; & 'C:\Users\speed\AppData\Local\Microsoft\WindowsApps\python3.11.exe' 'c:\Users\speed\.vscode\extensions\ms-python.python-2023.16.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '56397' '--' 'C:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2\HelloWorld2.py <502416@student.fontys.nl c:; cd 'c:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2'; & 'C:\Users\speed\AppData\Local\Microsoft\WindowsApps\python3.11.exe' 'c:\Users\speed\.vscode\extensions\ms-python.python-2023.16.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '56391' '--' 'C:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2\HelloWorld2.py>
2023-10-12 14:42:22 +0200

    video white circle detection

--- Webcam test.py
+++ Webcam test.py
@@ -2,19 +2,72 @@ import numpy as np
 import cv2 as cv
 
 # Open the default camera (camera index 0)
+# cap = cv.VideoCapture(0)
+
+# # infinite loop that keeps the camera open
+# while True:
+#     # Capture a frame from the camera
+#     # ret is as a boolean value that represents if the frame capture was sucessful
+#     ret, frame = cap.read()
+
+#     # Display the frame
+#     cv.imshow('Video', frame)
+
+#     # Exit the loop if the 'q' key is pressed (closes the camera when q is clicked)
+#     if cv.waitKey(1) == ord('q'):
+#         break
+
+# # Release the camera and close all OpenCV windows
+# cap.release()
+# cv.destroyAllWindows()
+
+import cv2 as cv
+import numpy as np
+
+# Open the default camera (camera index 0)
 cap = cv.VideoCapture(0)
 
-# infinite loop that keeps the camera open
 while True:
     # Capture a frame from the camera
-    # ret is as a boolean value that represents if the frame capture was sucessful
     ret, frame = cap.read()
 
-    # Display the frame
-    cv.imshow('Video', frame)
+    if not ret:
+        break
 
-    # Exit the loop if the 'q' key is pressed (closes the camera when q is clicked)
-    if cv.waitKey(1) == ord('q'):
+    # Convert the frame to grayscale
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Apply Gaussian blur to reduce noise
+    blurred = cv.GaussianBlur(gray, (9, 9), 2)
+
+    # Use the Hough Circle Transform to detect circles in the frame
+    circles = cv.HoughCircles(
+        blurred,
+        cv.HOUGH_GRADIENT,
+        dp=1,
+        minDist=100,  # Adjust the minimum distance between circles
+        param1=50,
+        param2=30,   # Adjust this threshold for circle detection
+        minRadius=0,
+        maxRadius=100  # Adjust the maximum radius of the circles you want to detect
+    )
+
+    # Ensure that at least one circle was found
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+
+        for circle in circles[0, :]:
+            # Extract the center and radius of the circle
+            center_x, center_y, radius = circle[0], circle[1], circle[2]
+
+            # Draw the circle on the original frame
+            cv.circle(frame, (center_x, center_y), radius, (0, 255, 0), 2)
+
+    # Display the frame with detected circles
+    cv.imshow('Video with Circles', frame)
+
+    # Exit the loop if the 'q' key is pressed
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release the camera and close all OpenCV windows
