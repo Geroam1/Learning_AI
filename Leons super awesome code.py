e4a8d265c41268032ac4442073f7fdba902bec0a
Geroam1 c:; cd 'c:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2'; & 'C:\Users\speed\AppData\Local\Microsoft\WindowsApps\python3.11.exe' 'c:\Users\speed\.vscode\extensions\ms-python.python-2023.16.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '56397' '--' 'C:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2\HelloWorld2.py <502416@student.fontys.nl c:; cd 'c:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2'; & 'C:\Users\speed\AppData\Local\Microsoft\WindowsApps\python3.11.exe' 'c:\Users\speed\.vscode\extensions\ms-python.python-2023.16.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '56391' '--' 'C:\Users\speed\Desktop\VS Code Stuff\Uni VS Code Stuff\Github Repositories\HelloWorld2\HelloWorld2.py>
2023-12-01 13:29:17 +0100

    commit already

--- Leons super awesome code.py
+++ Leons super awesome code.py
@@ -1,8 +1,11 @@
+# imports required for reading the varible
 import os
 from DetectionFunctions import update_Circle, get_CircleBin
 
-
+# defines the file path for the text file
 Circle_Text_File_path = "CircleBin.txt"
+
+# defines the binary variable
 circle = get_CircleBin(Circle_Text_File_path)
 
 print(circle)
